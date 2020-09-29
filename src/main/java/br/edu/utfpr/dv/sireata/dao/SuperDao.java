76c2916e29fba1e7026508b4fba2b2769abbd014
Josiane Pepis <alves.a.josiane@gmail.com>
2020-09-29 19:52:50 -0300

    Criação dos outros métodos repetidos
    Os métodos criados na interface são usados em mais de uma classe.

--- src/main/java/br/edu/utfpr/dv/sireata/dao/SuperDao.java
+++ src/main/java/br/edu/utfpr/dv/sireata/dao/SuperDao.java
@@ -4,4 +4,7 @@ import java.sql.SQLException;
 
 public interface SuperDAO<T> {
   T buscarPorId(int id) throws SQLException;
+  List T listarPorAta(int idAta) throws SQLException;
+  int salvar(T t) throws SQLException;
+  void excluir(T t) throws Exception{
 }
